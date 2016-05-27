cf66f435294d44a66164ffafff79d2ac14ae2617
Apoorva97 <api.matchless@gmail.com>
2016-05-27 13:49:28 -0700

    Update SysAd_task1.sh

--- SysAd_task1.sh
+++ SysAd_task1.sh
@@ -9,12 +9,14 @@ mkdir My_dir
 //To create 100 files
 touch file{1..100}.txt
 
-//To set the size of all the files as 10kb
-for x in {1..100}; do dd if=/dev/zero of=./file$x.txt bs=1024 count=10; done
-
-//To randomly alot 16 characters to each of the file
 charSet="[alnum]"
-for x in {1..100}; do cat /dev/urandom |tr -cd '$charSet'| fold -w 16 | head -n 1 > ./file$x.txt; done
-
+//To set the size of all the files as 10kb
+//To set the size of all the files as 10kb file
 //To modify the access of the files as read-only
-for x in {1..100}; do chmod 400 ./file$x.txt; done
+// To change the date of creation
+
+for x in {1..100}; 
+do 
+dd if=/dev/zero of=./file$x.txt bs=1024 count=10 cat /dev/urandom |tr -cd '$charSet'| fold -w 16 | head -n 1 > ./file$x.txt 
+do chmod 400 ./file$x.txt touch -d "2 days ago" ./file$x.txt;  
+done
